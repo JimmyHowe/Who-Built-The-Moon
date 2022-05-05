440c82a7c6f95b93ef470e97156c1befe611be33
James Howe <James.Howe@sds.co.uk>
2022-05-05 14:28:03 +0100

    First draft

---
+++ WhoBuilt.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SkillsDevelopmentScotland.Demo.Functions
+{
+    public static class WhoBuilt
+    {
+        static Dictionary<string, string> projects = new Dictionary<string, string>();
+
+        [FunctionName("WhoBuilt")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest request,
+            ILogger log)
+        {
+            log.LogInformation("YELLOW !!!!!");
+
+            string project = request.Query["project"];
+            string role = request.Query["role"];
+
+            string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
+            
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            
+            project = project ?? data?.project;
+            role = role ?? data?.role;
+
+            string responseMessage = $"Project: {project}. Role: {role}.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
